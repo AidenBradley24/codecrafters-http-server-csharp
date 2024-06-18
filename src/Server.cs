e3950b40090c31bd58563dd01d867c16dfa5ed63
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-06-18 00:13:47 -0400

    http requests and response 1

--- src/Server.cs
+++ src/Server.cs
@@ -2,10 +2,21 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 using var socket = server.AcceptSocket();
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+
+byte[] buffer = new byte[1024];
+socket.Receive(buffer);
+string request = Encoding.UTF8.GetString(buffer);
+string[] sections = request.Split("\r\n");
+
+string[] requestLine = sections[0].Split(' ');
+string method = requestLine[0];
+string url = requestLine[1];
+string httpVersion = requestLine[2];
+
+string message = url == "/" ? "200 OK" : "404 Not Found";
+string response = $"{httpVersion} {message}\r\n\r\n";
+Console.WriteLine(response);
+socket.Send(Encoding.UTF8.GetBytes(response));
